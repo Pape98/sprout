cd8cc1dced1657b4e0f07989465503c5828ad772
Pape98 <papisline2222@gmail.com>
2021-12-03 20:16:57 -0500

    Initial Commit

---
+++ community-garden-ios/community_garden_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  community_garden_iosApp.swift
+//  community-garden-ios
+//
+//  Created by Pape Sow Traore on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct community_garden_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
