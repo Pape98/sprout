38fdfeede912cec829b03fd3c7586165ea0af821
Pape98 <papisline2222@gmail.com>
2021-12-04 22:23:59 -0500

    - fetched steps successfully

--- community-garden-ios/Services/HealthStore.swift
+++ community-garden-ios/Services/HealthStore.swift
@@ -40,38 +40,57 @@ class HealthStore {
         }
     }
     
-    func startObserverQuery(dataType: HKSampleType) -> Void {
+    func startQuery(dataType: HKQuantityType) -> Void {
+
+        let calendar = Calendar.current
+
+        // Create a 1-day interval.
+        let interval = DateComponents(day: 1)
         
-        healthStore?.enableBackgroundDelivery(for: dataType, frequency: HKUpdateFrequency.immediate) { success, error in
-            guard error == nil else {
-                print(error!)
-                return
+        // Set the anchor for 0 a.m. on Monday.
+        
+        let components = DateComponents(calendar: calendar,
+                                        timeZone: calendar.timeZone,
+                                        hour: 0,
+                                        minute: 0,
+                                        second: 0,
+                                        weekday: 2) // Number 2 represents monday
+
+        guard let anchorDate = calendar.nextDate(after: Date(),
+                                                 matching: components,
+                                                 matchingPolicy: .nextTime,
+                                                 repeatedTimePolicy: .first,
+                                                 direction: .backward) else {
+            fatalError("*** unable to find the next Monday. ***")
+        }
+        
+        let query = HKStatisticsCollectionQuery(quantityType: dataType,
+                                                quantitySamplePredicate: nil,
+                                                options: .cumulativeSum,
+                                                anchorDate: anchorDate,
+                                                intervalComponents: interval)
+        
+        query.initialResultsHandler = {
+            query, results, error in
+            guard let statsCollection = results else {
+                 assertionFailure("")
+                 return
+             }
+            
+            let twoDaysAgo = DateComponents(day: -2)
+            
+            guard let startDate = calendar.date(byAdding: twoDaysAgo, to: Date()) else {
+                fatalError("*** Unable to calculate the start date ***")
+            }
+            
+            statsCollection.enumerateStatistics(from: startDate, to: Date()){ statistics, stop in
+                print(statistics.sumQuantity())
             }
         }
         
-        let observerQuery = HKObserverQuery.init(sampleType: dataType, predicate: nil) { (query, completionHandler, error) in
-            guard error == nil else {
-                print(error!)
-                return
-            }
-            
-            let sampleQuery = HKSampleQuery(sampleType: dataType, predicate: nil, limit: Int(HKObjectQueryNoLimit), sortDescriptors: nil){
-                query, results, error in
-            
-                guard let samples = results as? [HKQuantitySample] else {
-                    return
-                }
-                
-                print(samples.count)
-                
-            }
-            
-            self.healthStore?.execute(sampleQuery)
-            
-            completionHandler()
+        if let healthStore = self.healthStore {
+            healthStore.execute(query)
         }
-        
-        healthStore?.execute(observerQuery)
         
     }
 }
--- community-garden-ios/ViewModels/HealthModel.swift
+++ community-garden-ios/ViewModels/HealthModel.swift
@@ -15,7 +15,7 @@ class HealthModel: ObservableObject {
         if let healthStore = healthStore {
             healthStore.requestAuthorization { success in
                 if success {
-                    healthStore.startObserverQuery(dataType: HKDataTypes.stepCount)
+                    healthStore.startQuery(dataType: HKDataTypes.stepCount)
                 }
             }
         }
