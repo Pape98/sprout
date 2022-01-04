e28379e134bfd0ac9ab5e8fdb62d1a85a8d324f2
Pape98 <papisline2222@gmail.com>
2022-01-03 21:10:15 -0500

    put howAreYou Notification in its own method

--- community-garden-ios/Services/NotificationService.swift
+++ community-garden-ios/Services/NotificationService.swift
@@ -10,11 +10,13 @@ import UserNotifications
 
 class NotificationService {
     
+    var center: UNUserNotificationCenter
+    
     init() {
         // Request authorization to send local notifications (for reminder purposes)
-        let center = UNUserNotificationCenter.current()
+        center = UNUserNotificationCenter.current()
         
-        // Step 1: Ask for permission
+        // Ask for permission
         center.requestAuthorization(options: [.alert, .sound]) { isGranted, error in
             guard error == nil else {
                 print("requestAuthorizations()", error!)
@@ -22,34 +24,37 @@ class NotificationService {
             }
         }
         
-        // Step 2: Create the notification content
+        setupHowAreYouNotification()
+    }
+    
+    func setupHowAreYouNotification() {
+        // Step 1: Create the notification content
         let content = UNMutableNotificationContent()
         content.title = "Message from Community Garden"
         content.body = "Hello there! How are you feeling?"
         content.sound = UNNotificationSound.default
         
-        // Step 3: Create the notification trigger
+        // Step 2: Create the notification trigger
 
-        let date =  "21:02:00".convertToDateObject(format: "HH:mm:ss")
+        let date =  "21:10:00".convertToDateObject(format: "HH:mm:ss")
         let dateComponents = Calendar.current.dateComponents([.hour,.minute, .second], from: date!)
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents,
                                                     repeats: true)
             
-        // Step 4: Create the request
+        // Step 3: Create the request
         let uuidString = UUID().uuidString
         let request = UNNotificationRequest(identifier: uuidString,
                                             content: content,
                                             trigger: trigger)
     
-        // Step 5: Register with notification center
+        // Step 4: Register with notification center
         center.add(request) { error in
             
             if let error = error {
                 print("[NOTIFICATION ERROR] ", error)
             }
         }
-        
     }
 }
  
