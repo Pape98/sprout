866f14797c1ecef3d8f69597772d441deb312a50
Pape98 <papisline2222@gmail.com>
2022-06-07 01:27:34 -0400

    reorganized dashboard components

---
+++ community-garden-ios/Views/Screens/Dashboard/DashboardCard.swift
@@ -0,0 +1,44 @@
+//
+//  DashboardCard.swift
+//  community-garden-ios
+//
+//  Created by Pape Sow Traore on 6/7/22.
+//
+
+import SwiftUI
+
+struct DashboardCard<Content: View>: View {
+    
+    var width: CGFloat
+    var icon: String
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        
+        content
+            .frame(width: width, height: 60)
+            .padding(.vertical, 20)
+            .background{
+                ZStack (alignment: .topLeading) {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(10)
+                        .opacity(0.9)
+                    
+                    Image(icon)
+                        .padding(10)
+                }
+                
+                
+            }
+    }
+}
+
+struct DashboardCard_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardCard(width: 100, icon: "calendar-icon") {
+            Text("Card")
+                .headerStyle()
+        }
+    }
+}
---
+++ community-garden-ios/Views/Screens/Dashboard/GardenInfoCard.swift
@@ -0,0 +1,55 @@
+//
+//  GardenInfoCard.swift
+//  community-garden-ios
+//
+//  Created by Pape Sow Traore on 6/7/22.
+//
+
+import SwiftUI
+
+struct GardenInfoCard: View {
+    var body: some View {
+        
+        ZStack(alignment: .leading) {
+            
+            // Steps
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("1247")
+                        .bold()
+                        .headerStyle()
+                    Text("Steps")
+                        .bold()
+                        .bodyStyle()
+                }
+                
+                Spacer()
+                
+                // Buttons
+                HStack(spacing: 10) {
+                    IconButton(icon: "droplet-icon", text: "5 droplets")
+                    IconButton(icon: "garden-icon", text: "Your Garden")
+                    
+                }
+                
+            }
+            .padding(25)
+            .background {
+                ZStack (alignment: .leading) {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(10)
+                        .opacity(0.9)
+                    
+                    Image("step-icon")
+                }
+            }
+        }
+    }
+}
+
+struct GardenInfoCard_Previews: PreviewProvider {
+    static var previews: some View {
+        GardenInfoCard()
+    }
+}
