bb14af1d0f43939d8697e621faabef8e83f1fdb8
Pape Sow Traoré <papisline2222@gmail.com>
2022-08-19 10:41:09 -0400

    - switched to lab account 3

--- community-garden-ios/CommunityGardenIosApp.swift
+++ community-garden-ios/CommunityGardenIosApp.swift
@@ -24,7 +24,7 @@ struct CommunityGardenIosApp: App {
         //            print(defaults)
         //        }
         FirebaseApp.configure()
-        //setupLocalEmulator()
+        setupLocalEmulator()
         
     }
     
